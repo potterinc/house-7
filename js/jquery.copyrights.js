7fe528ed8cb65870ec3c0e03e9c517deecaebde5
potterinc <harry_p30@yahoo.com>
2020-06-10 01:41:10 +0100

    DONE: updated pictures and content

--- js/jquery.copyrights.js
+++ js/jquery.copyrights.js
@@ -1,5 +1,6 @@
 (function($){
 	
+	// copyrigths year
 	$.fn.copyrights = function(){
 		
 		// to use this for variable selectors
@@ -8,5 +9,12 @@
 			return $(this).html(d.getFullYear());
 		});
 	};
+	
+	// Developer
+	$.fn.developer = function(){
+		return this.each(function(){
+			$(this).html('<a href="https://potterincorporated.com" target="_blank">Potter Inc&trade;</a>');
+		})
+	}
 
 }(jQuery));
